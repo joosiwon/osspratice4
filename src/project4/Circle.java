53a32ec6024a1fdd65abe3641d067dc441636058
joosiwon <90442015+joosiwon@users.noreply.github.com>
2021-09-24 19:30:33 +0900

    initial commit

--- src/project4/Circle.java
+++ src/project4/Circle.java
@@ -1,5 +1,5 @@
 package project4;
-
+/*
 public class Circle {
 	int radius;
 	String name;
@@ -10,7 +10,8 @@ public class Circle {
 		return 3.14*radius*radius;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Circle pizza = new Circle();
 		pizza.radius = 10;
 		pizza.name = "자바피자";
@@ -23,11 +24,37 @@ public class Circle {
 		area = donut.getArea();
 		System.out.println(donut.name + "의 면적은"+ area);
 		
-		
-		
-		
-		// TODO Auto-generated method stub
-
 	}
+
+}
+*/
+
+public class Circle {
+int radius;
+String name;
+
+public Circle() 
+{ // 매개 변수 없는 생성자
+radius = 1; name = ""; // radius의 초기값은 1
+}
+public Circle(int r, String n)
+{ // 매개 변수를 가진 생성자
+radius = r; name = n;
+}
+public double getArea() 
+{
+return 3.14*radius*radius;
+}
+public static void main(String[] args)
+
+{
+Circle pizza = new Circle(10, "자바피자"); // Circle 객체 생성, 반지름 10
+double area = pizza.getArea();
+System.out.println(pizza.name + "의 면적은 " + area);
+Circle donut = new Circle(); // Circle 객체 생성, 반지름 1
+donut.name = "도넛피자";
+area = donut.getArea();
+System.out.println(donut.name + "의 면적은 " + area);
+}
 
 }
--- src/project4/Rectangel.java
+++ src/project4/Rectangel.java
@@ -1,8 +1,30 @@
 package project4;
 
-public class Rectangel {
+import java.util.Scanner;
 
+public class Rectangel {
+	int width;
+	int height;
+	
+	public int getArea() {
+		return width*height;
+	}
+	
+	
 	public static void main(String[] args) {
+		Rectangel rect = new Rectangel();
+		Scanner sc = new Scanner(System.in);
+		System.out.print(">> ");
+		
+		rect.width = sc.nextInt();
+		rect.height = sc.nextInt();
+		
+		System.out.println("사각형의 면적은 " + rect.getArea());
+		
+		sc.close();
+		
+		
+		
 		// TODO Auto-generated method stub
 
 	}
