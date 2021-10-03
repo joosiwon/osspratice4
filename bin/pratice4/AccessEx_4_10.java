7480d2241c9f2af75717ac1b96878b5218837684
joosiwon <90442015+joosiwon@users.noreply.github.com>
2021-10-03 23:17:03 +0900

    Add files via upload

---
+++ bin/pratice4/AccessEx_4_10.java
@@ -0,0 +1,18 @@
+package pratice4;
+
+
+class Sample {
+    public int a;
+    private int b;
+    int c;
+}
+public class AccessEx_4_10 {
+    public static void main(String[] args) {
+
+
+        Sample aClass = new Sample();
+        aClass.a = 10;
+       // aClass.b = 10;
+        aClass.c = 10;
+    }
+    }
---
+++ bin/pratice4/GarbageEx_4_9.java
@@ -0,0 +1,15 @@
+package pratice4;
+
+public class GarbageEx_4_9 {
+    public static void main(String[] args) {
+        String a = new String("Good");
+        String b = new String("Bad");
+        String c = new String("Normal");
+        String d, e;
+        a=null;
+        d=c;
+        c=null;
+
+
+    }
+}
